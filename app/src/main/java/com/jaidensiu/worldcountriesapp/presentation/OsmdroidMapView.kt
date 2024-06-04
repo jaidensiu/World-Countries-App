c9b4a151d4a23a8bb1cdbf5e75b1e983eb2cc83e
jaidensiu <jaidensiu@gmail.com>
2024-06-04 00:43:02 -0700

    feat: added custom marker for map

--- app/src/main/java/com/jaidensiu/worldcountriesapp/presentation/OsmdroidMapView.kt
+++ app/src/main/java/com/jaidensiu/worldcountriesapp/presentation/OsmdroidMapView.kt
@@ -1,10 +1,17 @@
 package com.jaidensiu.worldcountriesapp.presentation
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.jaidensiu.worldcountriesapp.BuildConfig
+import com.jaidensiu.worldcountriesapp.R
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
@@ -12,10 +19,7 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
 @Composable
-fun OsmMapView(
-    modifier: Modifier = Modifier,
-    geoPoint: GeoPoint
-) {
+fun OsmMapView(modifier: Modifier = Modifier, geoPoint: GeoPoint) {
     AndroidView(
         modifier = modifier.fillMaxSize(),
         factory = { context ->
@@ -34,9 +38,8 @@ fun OsmMapView(
                 )
                 setMultiTouchControls(true)
                 setUseDataConnection(true)
-                tileProvider.clearTileCache()
                 this.maxZoomLevel = 20.0
-                this.minZoomLevel = 4.0
+                this.minZoomLevel = 3.0
                 this.isHorizontalMapRepetitionEnabled = false
                 this.isVerticalMapRepetitionEnabled = false
                 this.controller.setCenter(geoPoint)
@@ -45,9 +48,21 @@ fun OsmMapView(
                 marker.setOnMarkerClickListener { _, _ -> true }
                 marker.setPosition(geoPoint)
                 marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+                val drawable = ContextCompat.getDrawable(context, R.drawable.ic_marker)
+                val resizedDrawable = resizeDrawable(context, drawable!!, 100, 100)
+                marker.icon = resizedDrawable
                 this.overlays.add(marker)
                 this.invalidate()
             }
         }
     )
+}
+
+fun resizeDrawable(context: Context, drawable: Drawable, width: Int, height: Int): Drawable {
+    val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    val resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
+    return BitmapDrawable(context.resources, resizedBitmap)
 }
